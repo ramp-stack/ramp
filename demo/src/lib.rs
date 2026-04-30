fd577852c11cd4bd567709cf2b43692f7d5f7911
CyberRubberDucky <eli@orange.me>
2026-04-30 13:43:32 +0200

    fixed airstuff in demo

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -382,21 +382,22 @@ impl App {
 
 #[derive(Serialize, Deserialize, Hash)]
 pub struct ChatRoom;
+impl ChatRoom {
+    pub fn new(_name: &str) -> Self {ChatRoom}
+}
 impl Contract for ChatRoom {
-    fn id() -> Id { Id::hash("ChatRoom2.5") }
+    fn id() -> Id {Id::hash("ChatRoom2.5")}
 
-    fn init(self, signer: &Name, _timestamp: u64) -> Substance {
-        Substance::Map(BTreeMap::from([
-            ("name".to_string(),     Substance::String("myroom".to_string())),
-            ("author".to_string(),   Substance::String(signer.to_string())),
-            ("messages".to_string(), Substance::map()),
-        ]))
-    }
+    fn init(self, signer: &Name, _timestamp: u64) -> Substance {Substance::Map(BTreeMap::from([
+        ("name".to_string(), Substance::String("myroom".to_string())),
+        ("author".to_string(), Substance::String(signer.to_string())),
+        ("messages".to_string(), Substance::map())
+    ]))}
 
     fn routes() -> BTreeMap<PathBuf, Reactants> {
         BTreeMap::from([
-            (PathBuf::from("/name"),     Reactants::new().add::<ChangeName>()),
-            (PathBuf::from("/messages"), Reactants::new().add::<SendMessage>()),
+            (PathBuf::from("/name"), Reactants::new().add::<ChangeName>()),
+            (PathBuf::from("/messages"), Reactants::new().add::<SendMessage>())
         ])
     }
 }
@@ -423,9 +424,9 @@ impl Reactant for SendMessage {
 
     fn apply<B: Beaker>(self, _path: &Path, signer: &Name, timestamp: u64, substance: &mut B) -> Result<(), Self::Error> {
         let _ = substance.insert("/messages/-", Substance::Map(BTreeMap::from([
-            ("author".to_string(),    Substance::String(signer.to_string())),
+            ("author".to_string(), Substance::String(signer.to_string())),
             ("timestamp".to_string(), Substance::Integer(timestamp as i64)),
-            ("body".to_string(),      Substance::String(self.0)),
+            ("body".to_string(), Substance::String(self.0)),
         ])));
         Ok(())
     }
